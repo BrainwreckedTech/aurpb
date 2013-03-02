4226d6aa7e7369838e8285f6b5ba9f499af3290e
BrainwreckedTech <paulhinchberger3+github@gmai.com>
2013-03-02 18:56:21 -0500

    First Real Commit

---
+++ anonymize.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This script replaces sensitive information in autobuild.sh
+
+sed -i 's/^repdir=.*/repdir=\[dir\]/g' autobuild.sh
+sed -i 's/^repnms=.*/repnsm=\[name\]/g' autobuild.sh
+sed -i 's/^rmuser=.*/rmuser=\[user\]/g' autobuild.sh
+sed -i 's/^rmhost=.*/rmhost=\[host\]/g' autobuild.sh
+sed -i 's/^rmport=.*/rmport=\[port\]/g' autobuild.sh
+sed -i 's/^rmpath=.*/rmport=\[path\]/g' autobuild.sh
