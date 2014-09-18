e34c4bcf5aa4904e72e5cdf40487851c0003e4e1
Paul A. Hinchberger III <paulhinchberger3@gmail.com>
2014-09-18 16:09:16 -0400

    Enforced curly braces on variables, changed column widths, program a little less verbose (-i reverts)

--- README.md
+++ README.md
@@ -18,17 +18,17 @@ First you will need some directories.
 By default, the script use /srv/repo.
 
 * /srv/repo
-    contains the scripts
+  - contains the scripts
 * /srv/repo/[name]
-    for each repositiory you want
+  - for each repositiory you want
 * /srv/repo/[name]/aur
-    temp space for buiding from the AUR
+  - temp space for buiding from the AUR
 * /srv/repo/[name]/aur/packages.list
-    which packages you want to build
-    can list multiple packages per line -- useful for dependencies
+  - which packages you want to build
+  - can list multiple packages per line -- useful for dependencies
 * /srv/repo/[name]/aur/[package-name].sh
-    Sometimes AUR packages need a little help
-    The .sh is a standard shelll script that can do anything you want 
+  - Sometimes AUR packages need a little help
+  - The .sh is a standard shelll script that can do anything you want 
 
 Chroot Setup
 ------------
@@ -46,3 +46,18 @@ https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot#Cl
 
 For building your i686 chroot:
 https://wiki.archlinux.org/index.php/Building_32-bit_packages_on_a_64-bit_system
+
+For each chroot, you will want to setup pacman.conf to reference your repository.
+
+Additional Setup Notes
+----------------------
+
+Be sure you build dependencies first!  The best way to catch dependencies is to go to the AUR package's page and hover over the links in the dependency section.  If the link points to www.archlinux.org, the dependency is in the official repos.  If the link points to aur.archlinux.org, you will have to build that package before you can build the package you want.  As is standard in Linux, dependencies can get quite deep.
+
+There is a bit of a race condition when building a repo for the first time.  If you correctly set up your new repo in the chroot first, it will fail to retreive the non-existent repo database.  If any package requires installing dependencies, this will cause the build process to fail becauase pacman will return an error code.  This can be resolved in two ways:
+
+* Make your first package one that does not have any dependencies.
+* Set up your new repo in the chroots AFTER building ONE package successfully.
+
+It is generally a good idea to keep "staging" and "production" versions of your repo.  Keep your chroots to pointed to the staging version and clients pointed at a seperate production version.  When all goes well in staging, copy new files to production first, then the new database files, followed by deletion of old files.  If something goes awry and packages get borked, copy them back over from production instead of building new ones.
+
--- makepkgs.sh
+++ makepkgs.sh
@@ -2,7 +2,7 @@
 
 show_help () {
 /usr/bin/cat <<END_OF_SHOW_HELP
-$0 <OPTIONS>
+${0} <OPTIONS>
 
 FLAGS THAT ONLY MAKE SENSE BY THEMSELVES:
 
@@ -21,6 +21,8 @@ COMBINABLE FLAGS:
                    Program will build i686 packages in <CHROOT-DIR>/i686
                    and build x86_64 packages in <CHROOT-DIR>/x86_64.
 
+  -i               Prints extra information.  (Not exactly verbose.)
+
   -n  <REPO-NAME>  Repository name.  Will be appended to <REPO-DIR>.
 
   -r  <REPO-DIR>   Repository directory.  Default is /srv/repo.
@@ -28,7 +30,7 @@ COMBINABLE FLAGS:
 
   -u  <USERNAME>   User to run package signing as.  Defaults to \$USER,
                    which retains orignal user name even with privilege
-                   escalation with sudo or su.
+                   escalation with su.
   
   -f  Update RSS   Update an RSS feed
 
@@ -39,9 +41,9 @@ END_OF_SHOW_HELP
 
 COLOR='\e[1;35m'
 RESET='\e[0m'
-TAB1='tput cub 80; tput cuf 28'
-TAB2='tput cub 80; tput cuf 49'
-TAB3='tput cub 80; tput cuf 70'
+TAB1='tput cub 80; tput cuf 30'
+TAB2='tput cub 80; tput cuf 55'
+TAB3='tput cub 80; tput cuf 80'
 
 ### GLOBAL VARIABLES ###
 
@@ -52,19 +54,21 @@ FLAG_LIST=false # Only list package info?
 FLAG_REPO=false # Only update the repos?
 FLAG_SIGN=false # Only sign packages?
 FLAG_URSS=false # Update the RSS feed?
+FLAG_INFO=false # Print a little more info?
 
 ### SET VARIABLES BASED ON PARAMETERS ###
 
 OPTIND=1
 
-while getopts "hldsfc:r:n:u:" opt; do
-  case "$opt" in
+while getopts "hildsfc:r:n:u:" opt; do
+  case "${opt}" in
     '?')  show_help >&2 && exit 1;;
     h) show_help && exit 0;;
     l) FLAG_LIST=true ; FLAG_MAKE=false ; break ;;
     d) FLAG_REPO=true ; FLAG_MAKE=false ; break ;;
     s) FLAG_SIGN=true ; FLAG_MAKE=false ; break ;;
     f) FLAG_URSS=true ; FLAG_MAKE=true ;;
+    i) FLAG_INFO=true   ;;
     c) CHROOT=${OPTARG} ;;
     r) REPDIR=${OPTARG} ;;
     n) REPNAM=${OPTARG} ;;
@@ -74,29 +78,29 @@ done
 
 shift $((OPTIND-1))
 
-[ -z ${CHROOT} ] && CHROOT="/srv/build" && echo "No chroot directory specified, defaulting to /srv/build" 
-[ -z ${REPDIR} ] && REPDIR="/srv/repo" && echo "No repo directory specified, defaulting to /srv/repo"
-[ -z ${USRNAM} ] && USRNAM="${USER}" && echo "No username specified.  Will sign packages as ${USER}"
+[ -z ${CHROOT} ] && CHROOT="/srv/build" && $FLAG_INFO && echo "No chroot directory specified, defaulting to /srv/build" 
+[ -z ${REPDIR} ] && REPDIR="/srv/repo" && $FLAG_INFO && echo "No repo directory specified, defaulting to /srv/repo"
+[ -z ${USRNAM} ] && USRNAM="${USER}" && $FLAG_INFO && echo "No username specified.  Will sign packages as ${USER}"
 [ -z ${REPNAM} ] && echo "No repo name specified" >&2 && show_help >&2 && exit 1
 
 ### CHECK TO SEE IF RUN AS ROOT ###
 
-[[ $EUID -eq 0 && ! $FLAG_MAKE ]] &&
+[[ ${EUID} -eq 0 && ! ${FLAG_MAKE} ]] &&
   echo "This script must be run as root when making packages." >&2 && exit 1
 
 ### SANITY CHECKS ###
 
 for binary in sed tar xz wget arch-nspawn makechrootpkg; do
-  type $binary > /dev/null 2>&1 || { echo >&2 "$binary is not installed."; exit 1; }
+  type ${binary} > /dev/null 2>&1 || { echo >&2 "${binary} is not installed."; exit 1; }
 done
 
-if [ ! -f "$REPDIR/$REPNAM/build/aur/packages.list" ]; then
-  echo >&2 "$REPDIR/$REPNAM/build/aur/packages.list does not exist."
+if [ ! -f "${REPDIR}/${REPNAM}/build/aur/packages.list" ]; then
+  echo >&2 "${REPDIR}/${REPNAM}/build/aur/packages.list does not exist."
   exit 1
 fi
 
-mkdir -p $REPDIR/$REPNAM/build/aur
-mkdir -p $REPDIR/$REPNAM/{x86_64,i686}
+mkdir -p ${REPDIR}/${REPNAM}/build/aur
+mkdir -p ${REPDIR}/${REPNAM}/{x86_64,i686}
 
 ### FUNCTIONS ###
 
@@ -120,10 +124,10 @@ function pkg_ver_comp () {
 }
 
 function pkg_ver_loc () {
-  lpkgnam=`ls ${REPDIR}/${REPNAM}/${2}/${1}-*.pkg.tar.xz 2> /dev/null | head -1 | rev | cut -d/ -f1 | rev`
+  lpkgnam=`ls ${REPDIR}/${REPNAM}/${2}/${1}-[0-9rl]*.pkg.tar.xz 2> /dev/null | head -1 | rev | cut -d/ -f1 | rev`
   lpkgver=`echo ${lpkgnam:\`expr ${#1} + 1\`:\`expr ${#lpkgnam} - ${#1} - 12\`} | rev | cut -d- -f2- | rev`
-  [[ "$lpkgnam" == "" ]] && lpkgver='missing'
-  echo $lpkgver
+  [[ "${lpkgnam}" == "" ]] && lpkgver='missing'
+  echo ${lpkgver}
 }
 
 function pkg_ver_aur () {
@@ -137,17 +141,17 @@ function pkg_ver_aur () {
 }
 
 function pkg_get () {
-  message "Retreiving gzipped tarball for ${1}..."
+  #message "Retreiving gzipped tarball for ${1}..."
   wget -q https://aur.archlinux.org/packages/${1:0:2}/${1}/${1}.tar.gz
-  message "Extracting gzipped tarballl for ${1}..."
+  #message "Extracting gzipped tarballl for ${1}..."
   tar -zxvf ${1}.tar.gz > /dev/null
-  message "${COLOR}Removing gzipped tarballl for ${1}..."
+  #message "${COLOR}Removing gzipped tarballl for ${1}..."
   rm ${1}.tar.gz
 }
 
 function pkg_remove() {
   message "Removing ${1}-${2} from ${3}..."
-  eval rm -rv "$REPDIR/$REPNAM/${3}/${1}-${2}-*.pkg.tar.xz*"
+  eval rm -rv "${REPDIR}/${REPNAM}/${3}/${1}-${2}-*.pkg.tar.xz*"
 }
 
 function pkg_add () {
@@ -160,7 +164,7 @@ function pkg_add () {
 function repo_build() {
   message "Updating repo database for ${1}..."
   rm ${REPDIR}/${REPNAM}/${1}/${REPNAM}.db*
-  if [ $EUID == `id -u ${USRNAM}` ]; then
+  if [ ${EUID} == `id -u ${USRNAM}` ]; then
     repo-add -q ${REPDIR}/${REPNAM}/${1}/${REPNAM}.db.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz
   else
     su -c "repo-add -q ${REPDIR}/${REPNAM}/${1}/${REPNAM}.db.tar.xz ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz" - ${USRNAM}
@@ -168,12 +172,12 @@ function repo_build() {
 }
 
 function sign_pkgs() {
-  message "Signing packages for ${1} as ${USRNAM}..."
+  message "Signing packages with missing signatures for ${1} as ${USRNAM}..."
   for file in ${REPDIR}/${REPNAM}/${1}/*.pkg.tar.xz; do
-    if [ ! -e $file.sig ]; then
+    if [ ! -e ${file}.sig ]; then
       echo "Signing ${file}..."
       if [ ${EUID} != `id -u ${USRNAM}` ]; then
-        su -c "gpg --detach-sign $file" - ${USRNAM}
+        su -c "gpg --detach-sign ${file}" - ${USRNAM}
       else
         gpg --detach-sign ${file}
       fi
@@ -184,51 +188,51 @@ function sign_pkgs() {
 function pkg_build () {
 
   message "Preparing to build ${1} for ${4}..."
-  rm -rf $REPDIR/$REPNAM/build/aur/${1}
+  rm -rf ${REPDIR}/${REPNAM}/build/aur/${1}
 
   mpec=1
   pkg_get ${1}
 
-  trch="`cat $REPDIR/$REPNAM/build/aur/${1}/PKGBUILD | grep arch= | cut -d= -f2`"
-  tnat="`echo $trch | grep ${4}`"
-  tany="`echo $trch | grep any`"
+  trch="`cat ${REPDIR}/${REPNAM}/build/aur/${1}/PKGBUILD | grep arch= | cut -d= -f2`"
+  tnat="`echo ${trch} | grep ${4}`"
+  tany="`echo ${trch} | grep any`"
 
-  if [ -n "$tany" -a "${4}" == "i686" ]; then
+  if [ -n "${tany}" -a "${4}" == "i686" ]; then
     message 'ANY package detected, will copy x86_64 version.'
-    cp $REPDIR/$REPNAM/x86_64/${1}* $REPDIR/$REPNAM/i686/
+    cp ${REPDIR}/${REPNAM}/x86_64/${1}* ${REPDIR}/${REPNAM}/i686/
     [[ "${2}" != "missing" ]] && pkg_remove ${1} ${2} ${4}
   else
-    if [ -n "$tnat" -o -n "$tany" ]; then
-      chown -R nobody $REPDIR/$REPNAM/build/aur/${1}
-      cd $REPDIR/$REPNAM/build/aur/${1}
+    if [ -n "${tnat}" -o -n "${tany}" ]; then
+      chown -R nobody ${REPDIR}/${REPNAM}/build/aur/${1}
+      cd ${REPDIR}/${REPNAM}/build/aur/${1}
       [[ -f ../${1}.sh ]] && message 'Executing PKGBUILD customization...' && sh ../${1}.sh
-      makechrootpkg -cur ${CHROOT}/${4}; mpec=$?
-      if [ $mpec == 0 ]; then
+      makechrootpkg -cur ${CHROOT}/${4}
+      if [ $? == 0 ]; then
         message 'Package creation succeeded!'
-        if [ -f "`ls $REPDIR/$REPNAM/build/aur/${1}/${1}-*.pkg.tar 2> /dev/null`" ]; then 
+        if [ -f "`ls ${REPDIR}/${REPNAM}/build/aur/${1}/${1}-*.pkg.tar 2> /dev/null`" ]; then 
           message 'Package left as tarball.  Manually compressing...'
-          xz $REPDIR/$REPNAM/build/aur/${1}/${1}-*.pkg.tar
+          xz ${REPDIR}/${REPNAM}/build/aur/${1}/${1}-*.pkg.tar
         fi 
         [[ "${2}" != "missing" ]] && pkg_remove ${1} ${2} ${4}
         pkg_add ${1} ${4}; sign_pkgs ${4}; repo_build ${4}; system_update ${4};
         NEWPKS="${NEWPKS}${1} for ${4}"$'\n'
-        cd $REPDIR/$REPNAM/build
+         cd ${REPDIR}/${REPNAM}/build
       else
         message 'Package creation failed!'
         BADPKS="${BADPKS}${1} for ${4}"$'\n'
       fi
-      cd $REPDIR/$REPNAM/build/aur
+      cd ${REPDIR}/${REPNAM}/build/aur
     else
       echo -e "\e[1;31mPACKAGE ${1} not intended for ${4}.${RESET}"
     fi
-    rm -rf $REPDIR/$REPNAM/build/aur/${1}
+    rm -rf ${REPDIR}/${REPNAM}/build/aur/w${1}
   fi
-  return $mpec
+  return ${mpec}
 }
 
 function pkg_search () {
-  pmsrch=`pacman -Ss ${1} | grep ${1} | grep -v $REPNAM | cut -f1 -d/`
-  if [ "$pmsrch" == "" ]; then
+  pmsrch=`pacman -Ss ${1} | grep ${1} | grep -v ${REPNAM} | cut -f1 -d/`
+  if [ "${pmsrch}" == "" ]; then
     echo -e "${COLOR}Inv Pkg${RESET}"
   else
     message "${1} is now in ${pmsrch:0:10}"
@@ -236,49 +240,75 @@ function pkg_search () {
   fi
 }
 
-$FLAG_SIGN && { sign_pkgs x86_64; sign_pkgs i686; exit 0; }
-$FLAG_REPO && { repo_build x86_64; repo_build i686; exit 0; }
-! $FLAG_LIST && system_update x86_64 && system_update i686
+${FLAG_SIGN} && { sign_pkgs x86_64; sign_pkgs i686; exit 0; }
+${FLAG_REPO} && { repo_build x86_64; repo_build i686; exit 0; }
+${FLAG_LIST} || { system_update x86_64 && system_update i686; }
 
-echo -ne PACKAGE NAME; eval $TAB1
-echo -ne LCL X86_64 VER; eval $TAB2
-echo -ne LCL I686 VER; eval $TAB3
-echo -e AUR VERSION
+echo -ne "PACKAGE NAME"; eval ${TAB1}
+echo -ne "LCL X86_64 VER"; eval ${TAB2}
+echo -ne "LCL I686 VER"; eval ${TAB3}
+echo -e  "AUR VERSION"
 
 while read line; do
   depupd=0
-  for pkg in $line; do
+  for pkg in ${line}; do
     [[ "${pkg:0:1}" == "#" ]] && break
-    cd $REPDIR/$REPNAM/build/aur
+    cd ${REPDIR}/${REPNAM}/build/aur
     lvx=$(pkg_ver_loc ${pkg} x86_64); lvi=$(pkg_ver_loc ${pkg} i686)
-    if [ $depupd == 1 ]; then
-      message "Dependency of $pkg updated.  Clearing out for rebuild..."
-      pkg_remove $pkg \* "{x86_64,i686}"
+    if [ ${depupd} == 1 ]; then
+      message "Dependency of ${pkg} updated.  Clearing out for rebuild..."
+      pkg_remove ${pkg} \* "{x86_64,i686}"
       repo_build x86_64; repo_build i686
       system_update x86_64; system_update i686
       lvx=$(pkg_ver_loc ${pkg} x86_64); lvi=$(pkg_ver_loc ${pkg} i686)
     fi
-    echo -ne ${COLOR}${pkg:0:27}${RESET}; eval $TAB1
-    echo -ne ${COLOR}$lvx${RESET}; eval $TAB2
-    echo -ne ${COLOR}$lvi${RESET}; eval $TAB3
-    av=$(pkg_ver_aur ${pkg}); echo -e ${COLOR}$av${RESET}
-    if [ $av == missing ]; then
-      pkg_search $pkg \* "{x86_64,i686}"
+    echo -ne "${COLOR}${pkg}${RESET}"; eval $TAB1
+    echo -ne "${COLOR}${lvx}${RESET}"; eval $TAB2
+    echo -ne "${COLOR}${lvi}${RESET}"; eval $TAB3
+    av=$(pkg_ver_aur ${pkg})
+    echo -e "${COLOR}${av}${RESET}"
+    if [ ${av} == missing ]; then
+      pkg_search ${pkg} \* "{x86_64,i686}"
       message "Removing ${1} from the repos..."
-      rm -rf $REPDIR/$REPNAM/build/aur/$pkg 2> /dev/null
+      rm -rf "${REPDIR}/${REPNAM}/build/aur/${pkg}" 2> /dev/null
     else
-      if [[ ${1} != '-l' ]]; then
-        [[ $lvx == missing ]] && pkg_build $pkg $lvx $av x86_64 && [[ $? == 0 ]] && depupd=1
-        pkg_ver_comp $lvx $av && pkg_build $pkg $lvx $av x86_64 && [[ $? == 0 ]] && depupd=1
-        [[ $lvi == missing ]] && pkg_build $pkg $lvi $av i686 && [[ $? == 0 ]] && depupd=1
-        pkg_ver_comp $lvi $av && pkg_build $pkg $lvi $av i686 && [[ $? == 0 ]] && depupd=1
+      if [ ${lvx} == missing ]; then
+        if [ ! ${FLAG_LIST} ]; then
+          pkg_build ${pkg} ${lvx} ${av} x86_64 && [[ $? == 0 ]] && depupd=1
+        else
+          $FLAG_INFO && echo "List mode on...not building missing x86_64 package."
+        fi
+      else 
+        if pkg_ver_comp ${lvx} ${av}; then
+          if [ ! ${FLAG_LIST} ]; then
+            pkg_build ${pkg} ${lvx} ${av} x86_64 && [[ $? == 0 ]] && depupd=1
+          else
+            echo "List mode on...not building out-of-date x_86_64 package."
+          fi
+        fi
+      fi
+      if [ ${lvi} == missing ]; then
+        if [ ! ${FLAG_LIST} ]; then
+          pkg_build ${pkg} ${lvi} ${av} x86_64 && [[ $? == 0 ]] && depupd=1
+        else
+          $FLAG_INFO && echo "List mode on...not building missing i686 package."
+        fi
+      else
+        if pkg_ver_comp ${lvi} ${av}; then
+          if [ ! ${FLAG_LIST} ]; then 
+            pkg_build ${pkg} ${lvi} ${av} x86_64 && [[ $? == 0 ]] && depupd=1
+          else
+            echo "List mode on...not building out-of-date i686 package."
+          fi
+        fi
       fi
     fi
   done
-done < $REPDIR/$REPNAM/build/aur/packages.list
+done < "${REPDIR}/${REPNAM}/build/aur/packages.list"
 
+echo -e "\e[1;32m*** FINISHED WITH REPO ${REPNAM} ***\e[0m"
 
-! $FLAG_URSS && exit 0 
+! ${FLAG_URSS} && exit 0 
 
 # This section relies on BrainwreckedRSS 
 # Visit rss.bw-tech.net for more information
